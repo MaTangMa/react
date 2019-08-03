e6a29968bfd569214f37e2894a40b2c06214ab41
machuan <machuan_mac@163.com>
2019-08-03 14:04:54 +0800

    [add] image css inport && click bind

--- NOTE.md
+++ NOTE.md
@@ -1,23 +1,24 @@
 ### 目录
-public 
+
+public
 
 src
-    index.js 入口文件
-        react 是核心库
-        registerServiceWorker.js 加快react运行速度的，现在这里没有
-        Reactdom 提供DOM相关功能
-        引入app.js 根组件
-    index.css 公共css 可以删除，以后在组件中写
-    app.js 根组件
-    app.text.js 测试文件
+index.js 入口文件
+react 是核心库
+registerServiceWorker.js 加快 react 运行速度的，现在这里没有
+Reactdom 提供 DOM 相关功能
+引入 app.js 根组件
+index.css 公共 css 可以删除，以后在组件中写
+app.js 根组件
+app.text.js 测试文件
 
-#### src新建文件
+#### src 新建文件
+
     components 组件文件夹
     assets 静态资源
 
+##### jsx 语法
 
-
-##### jsx语法
 ```
 render() {
     return (
@@ -29,3 +30,7 @@ render () {
     return <div></div>
 }
 ```
+
+- 绑定属性语法 react 关键词冲突
+  class 要用 className
+  lable 的 for 属性 用 htmlFor
--- src/components/News.js
+++ src/components/News.js
@@ -1,23 +1,44 @@
-import React from 'react'
-
-// 
+import React from 'react';
+// 图片引入
+import img from '../assets/images/red.jpg';
+//
+import '../assets/css/index.css';
 class News extends React.Component {
-    constructor (props) {
-        super (props) // props用于父子组件传值，也是标准
-        this.state = {
-            
-        }
-    }
-    render () {
-        return (
-            <div>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props); // props用于父子组件传值，也是标准
+    this.state = {
+      msg: '新闻',
+      newsList: [{ content: '1111' }, { content: '2222' }, { content: '3333' }],
+      htmlList: [<h2 key="1">h2list</h2>, <h2 key="2">h2list2</h2>]
+    };
+  }
+  render() {
+    let listResut = this.state.newsList.map((value, key) => {
+      return <li key={key}>{value.content}</li>;
+    });
+
+    return (
+      <div>
+        <ul>
+          <li>图片引入</li>
+          <li>远程图片引入</li>
+          <li>3</li>
+        </ul>
+        <img className="img" src={img} alt="" />
+        <img className="img" src={require('../assets/images/red.jpg')} alt="" />
+        <img className="img" src="https://www.baidu.com/img/bd_logo1.png?where=super" alt="" />
+
+        {this.state.htmlList}
+
+        <ul>{listResut}</ul>
+        <ul>
+          {// 或者直接在这里循环
+          this.state.newsList.map((value, key) => {
+            return <li key={key}>{value.content}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-export default News
+export default News;
